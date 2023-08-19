23b21a5b7a91c6812da5ff2727ee36b06bcf4d88
Sanjay Chihla <sanjaychihla@gmail.com>
2023-08-19 16:16:39 +0530

    Input player choice

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ let choiceNames = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
     console.log(choiceNames[(Math.floor(Math.random() * choiceNames.length))]);
 }
+
+const pSelection = prompt("Enter Your Selection");
